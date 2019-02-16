d100428cca984be6336868174543e5aa9cfca787
Qwertee <jonberrend@gmail.com>
2019-02-16 17:36:25 -0600

    updated form

--- Pollite/src/app/poll/new/new-poll-form/form/form.component.ts
+++ Pollite/src/app/poll/new/new-poll-form/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'new-poll-form',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  pollForm = this.fb.group({
+    pollName: ['', Validators.required],
+    pollOptions: this.fb.array([
+      this.fb.control('')
+    ], Validators.required)
+  });
 
-  constructor() { }
+  addOption() {
+    this.pollOptions.push(this.fb.control(''));
+  }
+
+  get pollOptions() {
+    return this.pollForm.get('pollOptions') as FormArray;
+  }
+
+  onSubmit() {
+
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  removeOption(i: number) {
+    this.pollOptions.removeAt(i);
+  }
 }
